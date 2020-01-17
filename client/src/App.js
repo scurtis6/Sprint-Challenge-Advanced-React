b386f013a6dd8658bbf7a3d8b048d4144ad1b8ab
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-17 13:12:22 -0500

    content is displaying

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
 import UserCard from './components/UserCard';
 
-class App extends React.Component {
-  state = {
-    name: ''
-  };
-
-  componentDidMount() {
-    axios
-    .get('http://localhost:5000/api/players')
-    .then(res => {
-      console.log(res.data)
-      this.setState({
-        name: res
-      });
-    })
-    .catch(err => console.log(err));
-  }
-
-  render () {
-    console.log('App is rendering');
-    return (
-      <div className="App">
-        <UserCard />
+function App () {
+  return (
+    <div>
+      <UserCard />
     </div>
-    );
-  }
+  )
 }
 
 export default App;
