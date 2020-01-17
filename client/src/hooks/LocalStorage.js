43461c037ca68eae32f7df2e1b794e0d86288db9
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-17 13:23:44 -0500

    adding custom hooks

---
+++ client/src/hooks/LocalStorage.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() =>{
+        if(window.localStorage.getItem(key)) {
+            return JSON.parse(window.localStorage.getItem(key));
+        } else {
+            window.localStorage.setItem(key, JSON.stringify(initialValue));
+            return initialValue;
+        }
+    });
+
+    const setValue = newValue => {
+        setStoredValue(newValue);
+        window.localStorage.setItem(key, JSON.stringify(newValue));
+    };
+
+    return [storedValue, setValue]
+}
