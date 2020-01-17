a9a72174a899fb5dd8afed762415bc159043e0fd
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-17 13:36:59 -0500

    finished adding hooks

--- DarkMode.js
+++ DarkMode.js
@@ -1,1 +1,23 @@
-import 
+import { useEffect } from 'react';
+import LocalStorage from './client/src/hooks/LocalStorage';
+
+const useDarkMode = (key, initialValue) => {
+    const [darkMode, setDarkMode] = LocalStorage(key, initialValue);
+
+    useEffect(() => {
+        if(darkMode === true){
+            document.body.classList.add('dark-mode')
+        } else {
+            document.body.classList.remove('dark-mode')
+        }
+    }, [darkMode]);
+
+    const toggleDarkMode = e => {
+        e.preventDefault();
+
+        return darkMode ? setDarkMode(false) : setDarkMode(true);
+    };
+    return [darkMode, setDarkMode, toggleDarkMode]
+};
+
+export default useDarkMode;
--- client/src/hooks/LocalStorage.js
+++ client/src/hooks/LocalStorage.js
@@ -2,12 +2,8 @@ import { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() =>{
-        if(window.localStorage.getItem(key)) {
-            return JSON.parse(window.localStorage.getItem(key));
-        } else {
-            window.localStorage.setItem(key, JSON.stringify(initialValue));
-            return initialValue;
-        }
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue 
     });
 
     const setValue = newValue => {
